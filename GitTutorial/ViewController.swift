f868410ec4ca01a5371cf6807842f95a7263f5d8
parto mac <partomac@partos-air-3.lan>
2019-01-12 09:28:48 +0330

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by parto design mac on 1/12/19.
+//  Copyright © 2019 parto design mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
